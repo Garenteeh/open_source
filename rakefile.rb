8801b5dc1941404ac42c5a714510b235bc5f5ea9
garenteeh <bmg.laurent@live.fr>
2023-12-01 16:06:25 +0100

    test

--- rakefile.rb
+++ rakefile.rb
@@ -1,2 +1,6 @@
 puts "hello world ! "
 puts "WHAT THIS ISN'T WORKING"
+desc 'Method #1: Use the default rake way to add two numbers and log the result'
+task :add, [:num1, :num] do |t, args|
+  puts args[:num1].to_i + args[:num].to_i
+end
